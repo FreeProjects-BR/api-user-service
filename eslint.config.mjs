b17b695b05074754f879a5b364a1ff57790812ae
Agnelo FH <tech.agnelo@gmail.com>
2024-09-02 20:15:17 -0300

    refactorinf eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+    },
+  },
 ];
