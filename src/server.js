47580a3e855f92eab1930fa27236cda93d998ea5
Agnelo FH <tech.agnelo@gmail.com>
2024-09-02 21:21:43 -0300

    added app.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,13 @@
 import dotenv from 'dotenv';
 import http from 'http';
+import app from './app.js';
 
 const env = process.env.NOD_ENV || 'development';
 dotenv.config({ path: `.env${env === 'production' ? '.production' : ''}` });
 
 const port = process.env.API_PORT || 801;
 
-const server = http.createServer();
+const server = http.createServer(app);
 server.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
