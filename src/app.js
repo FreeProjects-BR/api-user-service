48dcbe8d1611183a26fa3bec1d7be28ad5011bcf
Agnelo FH <tech.agnelo@gmail.com>
2024-09-04 21:57:28 -0300

    refactoring app.js and server.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import { errorHandler } from './middleware/errorHancler.js';
+import publicRoutes from './routes/public.js';
 
 const app = express();
 
 app.use(express.json());
-app.get('/api/user/service/message', (req, res) => {
+
+app.use('/api/user-service', publicRoutes);
+
+app.get('/api/user-service/message', (req, res) => {
   res.send('Olá, API User Service rodando na PORT 8001.');
 });
+
+app.use(errorHandler);
 
 export default app;
