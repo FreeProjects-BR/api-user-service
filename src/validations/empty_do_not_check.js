648877c27409015d832977f6e221a6c9b667d640
Agnelo FH <tech.agnelo@gmail.com>
2024-09-05 15:55:51 -0300

    added function checkEmptyToNot

---
+++ src/validations/empty_do_not_check.js
@@ -0,0 +1,12 @@
+export function checkEmptyToNot(value, fielName = 'O campo') {
+  if (value === undefined || value === null || value === '') {
+    return {
+      success: false,
+      message: `${fielName} não pode estar vazio.`,
+    };
+  }
+  return {
+    success: true,
+    message: `${fielName} está preenchido`,
+  };
+}
